7c914889c0bae86ab691786f50e7142f472d3909
waseemR02 <waseem.riaz.9999@gmail.com>
2023-04-28 01:13:09 +0530

    Add tasks.loop to check flag on callback from database change

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 import json
 
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
@@ -22,19 +22,41 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix=PREFIX,
                    description="Testing the bot", intents=intents)
+flag = False
 
-
+db_reference = db.reference('test/int')
 @bot.event
 async def on_ready():
     print(f"{bot.user.name} has connected to Discord!")
+    check_flag.start()
+    
 
 @bot.command(name='ping')
 async def ping(ctx):
     await ctx.send('pong')
+    await bot.get_channel(1101143451564781631).send("This is me again")
+
+async def send_message(channel: discord.TextChannel, message: str):
+    await channel.send(message)
+
+def on_value_change(event):
+    global flag
+    print(f"From callback: {flag}")
+    flag = True
+    # bot.loop.create_task(send_message(bot.get_channel(1101143451564781631), f"This changed {db_reference.get()}"))
+
+@tasks.loop(seconds=0.5)
+async def check_flag():
+    global flag
+    print(f"From loop: {flag}")
+    if flag:
+        await send_message(bot.get_channel(1101143451564781631), f"This changed {db_reference.get()}")
+        flag = False
 
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.CheckFailure):
         await ctx.send('You do not have the correct role for this command.')
-     
+
+db_reference.listen(on_value_change)
 bot.run(TOKEN, reconnect=True)
